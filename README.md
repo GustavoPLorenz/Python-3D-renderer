a46f0eecded69f3d59dbc6963935d7162d47ba1c
Gustavo Pozzebon Lorenz <guguplorenz@gmail.com>
2025-03-20 20:35:55 -0300

    Final

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python 3D renderer
  A 3D objects renderer using Python, Numpy and Pygame that renders any .obj archive.
+
+ Notes:
+    *I decided not to fill the polygons that form the object since I would have to use the painter's algorithm, which is not the best option in this case, or the Z-buffer, which, due to processing pixels individually, would end up being too slow for the project.
--- Renderer.py
+++ Renderer.py
@@ -2,6 +2,7 @@ import pygame
 import numpy as np
 import copy
 import time
+import random
 
 #Pygame init
 pygame.init()
@@ -20,98 +21,105 @@ def RotateObject(Object):
     SinX = np.sin(XTheta)
 
     RotatedObject = []
-    for Line in Object["Object"]:
-        RotatedLine = []
-        for Point in Line:
-            X = Point[0] - CameraX
-            Y = Point[1] - CameraY
-            Z = Point[2] - CameraZ
+    for Point in Object["Polygon"]:
+        X = Point[0] - CameraX
+        Y = Point[1] - CameraY
+        Z = Point[2] - CameraZ
 
-            YRotatedX = X * CosY + Z * SinY
-            YRotatedZ = -X * SinY + Z * CosY
+        YRotatedX = X * CosY + Z * SinY
+        YRotatedZ = -X * SinY + Z * CosY
 
-            XRotatedY = Y * CosX - YRotatedZ * SinX
-            XRotatedZ = Y * SinX + YRotatedZ * CosX
+        XRotatedY = Y * CosX - YRotatedZ * SinX
+        XRotatedZ = Y * SinX + YRotatedZ * CosX
 
-            RotatedLine.append((YRotatedX, XRotatedY, XRotatedZ))
-        RotatedObject.append(RotatedLine)
+        RotatedObject.append((YRotatedX, XRotatedY, XRotatedZ))
     return RotatedObject
 
 def Object2D(MovedObject):
-    Object2D = []
-    for Line in MovedObject["Object"]:
-        Line2D = []
-        if Line[0][2] * Line[1][2] < 0:
+    Polygon2D = []
+    ClippedPolygon = []
+    Polygon = MovedObject["Polygon"]
+    for Line in range(len(Polygon)):
+        Point1 = Polygon[Line]
+        Point2 = Polygon[(Line+1) % len(Polygon)]
 
-            Line[0] = list(Line[0])
-            Line[1] = list(Line[1])
+        if Point1[2] >= 0:
+            ClippedPolygon.append(Point1)
 
-            T = Line[0][2] / (Line[0][2] - Line[1][2])
+        if Point1[2] * Point2[2] < 0:
+            T = Point1[2] / (Point1[2] - Point2[2])
+            NewX = Point1[0] + T * (Point2[0] - Point1[0])
+            NewY = Point1[1] + T * (Point2[1] - Point1[1])
+            NewZ = 0
 
-            YRotatedX = Line[0][0] + T * (Line[1][0] - Line[0][0])
-            NewY = Line[0][1] + T * (Line[1][1] - Line[0][1])
-
-            if Line[0][2] < 0:
-                Line[0] = [YRotatedX, NewY, 0]
-            else:
-                Line[1] = [YRotatedX, NewY, 0]
-
-        for Point in Line:
+            ClippedPolygon.append((NewX,NewY,NewZ))
             
-            X = Point[0]
-            Y = Point[1]
-            Z = (Point[2] +0.01)/250
+    for Point in ClippedPolygon:
+            
+        X = Point[0]
+        Y = Point[1]
+        Z = (Point[2] +0.01)/250
 
-            W = Z/D
-            if(W > 0):
-                Line2D.append((((X*F)/W)+300,-((Y*F)/W)+300))
-        Object2D.append(Line2D)
-    return Object2D
+        W = Z/D
+        if(W > 0):
+            Polygon2D.append((((X*F)/W)+300,-((Y*F)/W)+300))
+    return Polygon2D
 
 def DrawObject(Object2D):
-    for Line in Object2D["Object"]:
-        try:
-            pygame.draw.polygon(Window,Object2D["Color"],Line, 1)
-        except:
-            pass
+    try:
+        pygame.draw.polygon(Window,Object2D["Color"],Object2D["Polygon"],1)
+    except:
+        pass
+#Transform .obj in list
+Polygons = []
+Vertex = []
+Normals = []
+Colors = {}
+LastColor = (255,255,255)
+LastColorName = ""
 
+MtlArchive = open("Chevrolet_Camaro_SS_Low.mtl")
+MtlContent = MtlArchive.read()
+Lines = MtlContent.split("\n")
+for Line in Lines:
+    if "newmtl " in Line:
+        Line = Line.split(" ")
+        LastColorName = Line[1]
+        
+    
+    if "Kd " in Line:
+        Line = Line.split(" ")
+        Colors[str(LastColorName)] = (int(float(Line[1])*255),int(float(Line[2])*255),int(float(Line[3])*255))
+
+ObjArchive = open("Chevrolet_Camaro_SS_Low.obj")
+ObjContent = ObjArchive.read()
+ObjContent = ObjContent.replace("  "," ")
+Lines = ObjContent.split("\n")
+for Line in Lines:
+    if "v " in Line:
+        Line = Line.split(" ")
+        Vertex.append((float(Line[1]),float(Line[2]),float(Line[3])))
+    
+    if "vn " in Line:
+        Line = Line.split(" ")
+        Normals.append((float(Line[1]), float(Line[2]), float(Line[3])))
+    
+    if "usemtl " in Line:
+        Line = Line.split(" ")
+        LastColor = Colors[str(Line[1])]
+
+    if "f " in Line:
+        ObjPolygon = {"Polygon":[],"Color":LastColor, "Normals":[]}
+        LineParts = Line.split(" ")
+        for Part in LineParts[1:]:
+            Part = Part.split("/")
+            ObjPolygon["Polygon"].append(Vertex[int(Part[0])-1])
+            ObjPolygon["Normals"].append(Normals[int(Part[2]) - 1])
+        Polygons.append(ObjPolygon)
 #Variable definitions
 Running = True
-Objects = [{}]
-
-#Transform .obj in list
-Archive = open("Car.obj")
-Content = Archive.read()
-Lines = Content.split("\n")
-Vertex = []
-for Line in Lines:
-    if len(Line) >= 1 :
-        LineParts = Line.split(" ")
-        if LineParts[0] == "v":
-            
-            X = float(LineParts[1])
-            Y = float(LineParts[2])
-            Z = float(LineParts[3])
-
-            Vertex.append((X,Y,Z))
-ObjObject = []
-for Line in Lines:
-    ObjLine = []
-    if len(Line) >= 1:
-        LineParts = Line.split(" ")
-        if LineParts[0] == "f":
-            for VertexPosition in LineParts[1:-1]:
-                VertexNumber = int(VertexPosition.split("/")[0]) - 1
-                ObjLine.append(Vertex[VertexNumber])
-            ObjObject.append(ObjLine)
-Objects[0] = {
-    "Name": "Teste",
-    "Color": (255,255,255),
-    "Object": ObjObject
-}
-            
-RotatedObjects = copy.deepcopy(Objects)
-Objects2D = copy.deepcopy(Objects)
+RotatedObjects = copy.deepcopy(Polygons)
+Objects2D = copy.deepcopy(Polygons)
 D = 1
 F = 1
 CameraX = 0
@@ -121,6 +129,7 @@ CameraYDegrees = 0
 CameraXDegrees = 0
 RotationVelocity = 45
 LastTime = pygame.time.get_ticks() / 1000
+DeltaTime = 1
 
 #Main loop
 while Running:
@@ -130,7 +139,7 @@ while Running:
             Running = False
     
     Keys = pygame.key.get_pressed()
-    move_speed = 0.01
+    move_speed = 1 * DeltaTime
     theta = np.radians(CameraYDegrees)
 
     forward_x = np.sin(theta) * move_speed
@@ -169,9 +178,10 @@ while Running:
         CameraXDegrees += RotationVelocity * DeltaTime
 
     #Functions calling
-    for i in range(len(Objects)):
-        RotatedObjects[i]["Object"] = RotateObject(Objects[i])
-        Objects2D[i]["Object"] = Object2D(RotatedObjects[i])
+    for i in range(len(Polygons)):
+        RotatedObjects[i]["Polygon"] = RotateObject(Polygons[i])
+        Objects2D[i]["Polygon"] = Object2D(RotatedObjects[i])
+        Objects2D[i]["Color"] = Polygons[i]["Color"]
         DrawObject(Objects2D[i])
 
     #Window settings
